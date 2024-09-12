9927a9fad5a41c3fcd88bd699ffce8b9bac95f00
namnvd <namnvdse150231@fpt.edu.vn>
2024-09-12 17:09:14 +0700

    nginx commit change port and enable proxy in nginx

--- route-service/src/main.ts
+++ route-service/src/main.ts
@@ -6,16 +6,7 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3003);
-
-  // const grpcApp = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-  //   transport: Transport.GRPC,
-  //   options: {
-  //     package: 'permission',
-  //     protoPath: './src/proto/permission.proto',
-  //   },
-  // });
-
-  // await grpcApp.listen();
+  await app.listen(3002);
+  
 }
 bootstrap();

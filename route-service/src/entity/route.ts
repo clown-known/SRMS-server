e63087a3a1b14774a81dac23a9475e7b42f390c4
CeasJ <kietlai667@gmail.com>
2024-10-23 09:11:19 +0700

    feat: Fetch all points without pagination

--- route-service/src/entity/route.ts
+++ route-service/src/entity/route.ts
@@ -20,11 +20,11 @@ export class Route extends BaseEntity {
     @Column({ nullable: true, type: "float" })
     estimatedTime: number;
 
-    @ManyToOne(() => Point, { nullable: false })
+    @ManyToOne(() => Point, (point) => point.startRoutes, { nullable: false, onDelete: 'CASCADE' })
     @JoinColumn({ name: 'start_point_id' })
     startPoint: Point;
 
-    @ManyToOne(() => Point, { nullable: false })
+    @ManyToOne(() => Point, (point) => point.endRoutes, { nullable: false, onDelete: 'CASCADE' })
     @JoinColumn({ name: 'end_point_id' })
     endPoint: Point;
 }
--- route-service/src/modules/point/point.controller.ts
+++ route-service/src/modules/point/point.controller.ts
@@ -1,42 +1,60 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from "@nestjs/common";
-import { PointService } from "./point.service";
-import { plainToInstance } from "class-transformer";
-import { CreatePointDTO } from "./dto/request/create-point.dto";
-import { UpdatePointDTO } from "./dto/request/update-point.dto";
-import { PageOptionsDto } from "src/common/pagination/page-option.dto";
-import { PageDto } from "src/common/pagination/page.dto";
-import { PointDTO } from "./dto/point.dto";
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { PointService } from './point.service';
+import { plainToInstance } from 'class-transformer';
+import { CreatePointDTO } from './dto/request/create-point.dto';
+import { UpdatePointDTO } from './dto/request/update-point.dto';
+import { PageOptionsDto } from 'src/common/pagination/page-option.dto';
+import { PageDto } from 'src/common/pagination/page.dto';
+import { PointDTO } from './dto/point.dto';
+import { PageMetaDto } from 'src/common/pagination/page-meta.dto';
 
 @Controller('points')
 export class PointController {
-    constructor(private readonly pointService: PointService) {}
+  constructor(private readonly pointService: PointService) {}
 
-    @Post()
-    async create(@Body() createPoint: CreatePointDTO): Promise<CreatePointDTO> {
-        return this.pointService.save(createPoint);
+  @Get('all')
+    async findAllWithoutPagination(): Promise<PointDTO[]> {
+        return this.pointService.findAllWithoutPagination();
     }
 
-    @Get()
-    async findAll(@Query() pageOptionsDto: PageOptionsDto): Promise<PageDto<PointDTO>> {
-        return this.pointService.findAll(pageOptionsDto);
-    }
+  @Post()
+  async create(@Body() createPoint: CreatePointDTO): Promise<CreatePointDTO> {
+    return this.pointService.save(createPoint);
+  }
 
-    @Get(':id')
-    async findOne(@Param('id') id: string): Promise<CreatePointDTO> {
-        const point = await this.pointService.findOne(id);
-        return plainToInstance(CreatePointDTO, point);
-    }
+  @Get()
+  async findAll(
+    @Query() pageOptionsDto: PageOptionsDto,
+  ): Promise<PageDto<PointDTO>> {
+    return this.pointService.findAll(pageOptionsDto);
+  }
 
-    @Patch(':id')
-    async update(@Param('id') id: string, @Body() updatePoint: UpdatePointDTO): Promise<CreatePointDTO> {
-        const updatedPoint = await this.pointService.updatePoint(id, updatePoint);
-        return plainToInstance(CreatePointDTO, updatedPoint);
-    }
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<CreatePointDTO> {
+    const point = await this.pointService.findOne(id);
+    return plainToInstance(CreatePointDTO, point);
+  }
 
-    @Delete(':id')
-    async remove(@Param('id') id: string): Promise<void> {
-        return this.pointService.removePoint(id);
-    }
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updatePoint: UpdatePointDTO,
+  ): Promise<CreatePointDTO> {
+    const updatedPoint = await this.pointService.updatePoint(id, updatePoint);
+    return plainToInstance(CreatePointDTO, updatedPoint);
+  }
 
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<void> {
+    return this.pointService.removePoint(id);
+  }
 }
